df75223e0d5acdd9e7e35a5a615b23d23c09a06e
João Donato <joaovictor_gd@hotmail.com>
2023-07-04 13:55:17 -0300

    Versão 1.2

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -10,50 +10,39 @@ public class Main {
         double n1;
         double n2;
         String op;
+        String retorno;
 
-        System.out.println("***Bem vindo a Calculadora***\n");
+        do {
+            System.out.println("***Bem vindo a Calculadora***\n");
 
-        System.out.print("Digite o primeiro número:");
-        n1 = entrada.nextDouble();
+            System.out.print("Digite o primeiro número:");
+            n1 = entrada.nextDouble();
 
-        System.out.print("Digite o segundo número:");
-        n2 = entrada.nextDouble();
+            System.out.print("Digite o segundo número:");
+            n2 = entrada.nextDouble();
 
-        System.out.print("Digite o operador:");
-        op = entrada.next();
+            System.out.print("Digite o operador:");
+            op = entrada.next();
 
-        if(op.equals("+")){
-            double resultado1 = n1 + n2;
-            System.out.println(resultado1);
-        }else if(op.equals("-")){
-            double resultado2 = n1 - n2;
-            System.out.println(resultado2);
-        }else if(op.equals("*")) {
-            double resultado3 = n1 * n2;
-            System.out.println(resultado3);
-        }else if(op.equals("/")) {
-            double resultado4 = n1 / n2;
-            System.out.println(resultado4);
-
-
-        entrada.close();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
+            if (op.equals("+")) {
+                double resultado1 = n1 + n2;
+                System.out.println(resultado1);
+            } else if (op.equals("-")) {
+                double resultado2 = n1 - n2;
+                System.out.println(resultado2);
+            } else if (op.equals("*")) {
+                double resultado3 = n1 * n2;
+                System.out.println(resultado3);
+            } else if (op.equals("/")) {
+                double resultado4 = n1 / n2;
+                System.out.println(resultado4);
+            }
+            System.out.println("Para finalizar digite 'C', ou se quiser retornar aperte qualquer tecla...");
+            retorno = entrada.next();
+        }while(retorno.equalsIgnoreCase("c"));
+        System.out.println("Obrigado...");
 
     }
 }
+
+
