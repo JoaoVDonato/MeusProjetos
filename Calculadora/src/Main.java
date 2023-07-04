7c49c3472ea7a63bc214e32ca46c88ba1b599a1f
João Donato <joaovictor_gd@hotmail.com>
2023-07-04 12:01:40 -0300

    Mensagem

---
+++ Calculadora/src/Main.java
@@ -0,0 +1,59 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double n1;
+        double n2;
+        String op;
+
+        System.out.println("***Bem vindo a Calculadora***\n");
+
+        System.out.print("Digite o primeiro número:");
+        n1 = entrada.nextDouble();
+
+        System.out.print("Digite o segundo número:");
+        n2 = entrada.nextDouble();
+
+        System.out.print("Digite o operador:");
+        op = entrada.next();
+
+        if(op.equals("+")){
+            double resultado1 = n1 + n2;
+            System.out.println(resultado1);
+        }else if(op.equals("-")){
+            double resultado2 = n1 - n2;
+            System.out.println(resultado2);
+        }else if(op.equals("*")) {
+            double resultado3 = n1 * n2;
+            System.out.println(resultado3);
+        }else if(op.equals("/")) {
+            double resultado4 = n1 / n2;
+            System.out.println(resultado4);
+
+
+        entrada.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
