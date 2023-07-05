cdafab56e78f9553956a1048c8b0a23f274957d3
João Donato <joaovictor_gd@hotmail.com>
2023-07-05 11:40:26 -0300

    Version 1.4.2

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -44,7 +44,7 @@ public class Main {
         System.out.println("Até mais!!!");
         System.out.println("Vamos alterar tudo");
         System.out.println("Vamos alterar tudo");
-        System.out.println("Vamos alterar tudo");
+        
 
     }
 }
